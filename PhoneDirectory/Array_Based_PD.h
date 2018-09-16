120f02b76d73a8339ad97880c35736fb787b1110
jtingstad21 <jtingstad21@my.whitworth.edu>
2018-09-16 16:51:25 -0700

    Finished 1.16 (JT)

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -53,24 +53,28 @@ public:
 	void save();
 
 private:
+	// Joe Tingstad
 	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
 	{
 	public:
 		Directory_Entry() {} // Default no-argument constructor 
 		Directory_Entry(std::string the_name, std::string the_number) {
-			// constructor not implemented yet
+			name = the_name;
+			number = the_number;
 		}
 		std::string get_name() const {
-			return ""; // method not implemented yet
+			return name;
 		}
 		std::string get_number() const {
-			return ""; // method not implemented yet
+			return number;
 		}
 		void set_number(const std::string& new_number) {
-			// method not implemented yet
+			number = new_number;
 		}
 
 	private:
+		string name;
+		string number;
 
 	};
 
